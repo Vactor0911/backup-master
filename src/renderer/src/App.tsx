8776a306f3937543b2345e915ad5ba945d246685
Vactor0911 <vactor0911@gmail.com>
2024-10-07 21:39:39 +0900

    initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,21 @@
+import { CssBaseline, Stack } from '@mui/material'
+
+function App(): JSX.Element {
+  return (
+    <>
+      <CssBaseline />
+      <Stack
+        width={'100vw'}
+        height={'100vh'}
+        css={{
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        Hello World!
+      </Stack>
+    </>
+  )
+}
+
+export default App
